d65de6fa0538b29a3c777d00a07bab603006527e
Alefe Eliel <candidoalefe@outlook.com>
2020-03-27 10:34:01 -0300

    agr é a ultima

--- src/routes.js
+++ src/routes.js
@@ -7,20 +7,21 @@ const LoginController = require('./Controllers/LoginController');
 const auth = require('./middleware/auth');
 const {validate} = require('./middleware/validator');
 const {ParticipantesValidationRules} = require('./validations/ParticipanteValidation');
+const {GruposValidationRules} = require('./validations/GrupoValidation')
 
 const routes = Router();
 
-routes.get('/participantes', ParticipantesController.index);
+routes.get('/participantes', auth, ParticipantesController.index);
 routes.get('/participantes/:email', auth, ParticipantesController.getParticipante);
-routes.post('/participantes', ParticipantesValidationRules(), validate, auth, ParticipantesController.post);
-routes.put('/participantes/:_id', ParticipantesController.edit);
-routes.delete('/participantes/:_id', ParticipantesController.delete);
+routes.post('/participantes', auth, ParticipantesValidationRules(), validate, ParticipantesController.post);
+routes.put('/participantes/:_id', auth, ParticipantesValidationRules(), validate, ParticipantesController.edit);
+routes.delete('/participantes/:_id', auth, ParticipantesController.delete);
 
-routes.get('/Grupos', GrupoController.index);
-routes.get('/Grupos/:_id', GrupoController.getGrupo);
-routes.post('/Grupos', GrupoController.create);
-routes.put('/Grupos/:_id', GrupoController.edit);
-routes.delete('/Grupos/:_id', GrupoController.delete);
+routes.get('/Grupos', auth, GrupoController.index);
+routes.get('/Grupos/:_id', auth,  GrupoController.getGrupo);
+routes.post('/Grupos', auth, GruposValidationRules(), validate, GrupoController.create);
+routes.put('/Grupos/:_id', auth, GruposValidationRules(), validate, GrupoController.edit);
+routes.delete('/Grupos/:_id', auth, GrupoController.delete);
 
 
 routes.post('/login', LoginController.geraToken);
